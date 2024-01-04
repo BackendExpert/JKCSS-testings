c870087ccbc9744600136ddca6360b667fd1d681
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-04 21:09:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
-    var main = document.querySelector('.main');
-
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-        main.style.marginLeft = '0';
-    } else {
-        sidebar.style.width = '250px';
-        main.style.marginLeft = '250px';
-    }
-}
+$('.sub-menu-btn').click(function(){
+    $(this).next('menu-sub').slideToggle();
+})
