d1ddb19ef3b354e35abc7030cff3a1812b009257
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-30 03:56:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- nmodal.js
+++ nmodal.js
@@ -1,2 +1,26 @@
-const modalBtn = document.querySelector("[data-target]");
-const btnClose = document.querySelector(".close-btn");
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
